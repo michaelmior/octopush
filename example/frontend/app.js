2dfc9e9d433e97016b40da8890c7b22f88bb73f2
adisbladis <adis@blad.is>
2013-05-30 21:41:44 +0200

    Changed example

--- example/frontend/app.js
+++ example/frontend/app.js
@@ -1,10 +1,5 @@
-var Push;
-
-$( document ).ready(function() {
-
-    Push = new Octopush("http://localhost:3001")
-    Push.on("my_event", function(data) {
-	$("div#messages").append("<p>" + JSON.stringify(data) + "</p>");
-    });
-
+var Push = new Octopush("http://localhost:3001");
+Push.on("my_event", function(data) {
+    $("div#messages").append("<p>" + JSON.stringify(data) + "</p>");
 });
+
