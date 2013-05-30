f04d147fa6ead52af77fbf596320a07a286e84e6
adisbladis <adis@blad.is>
2013-05-30 16:15:49 +0200

    Fixed client

--- usr/share/octopush/client.js
+++ usr/share/octopush/client.js
@@ -1,46 +1,57 @@
 function Octopush(url) {
 
+    this.events = {};
+    this.channels = [];
+
     //Base setup
     var that = this;
-    this.events = {},
-    this.channels = [],
-    this.socket = io.connect(url);
-    this.socket.on("message", function(data) {
-	if(typeof(that.events[data.e]) === "function")
-	    that.events[data.e](data.p);
-    });
-
-    //Internal states and error handling
-    this.socket.on("error", function(data) {
-	if(typeof(console) !== "undefined") {
-	    console.error(data);
-	}
-    });
-    this.socket.on("auth", function(data) {
-	if(typeof(console) !== "undefined") {
-	    switch(data) {
-
-	    case 0:
-		console.log("Authenticated");
-		break;
-		
-	    case 1:
-		console.error("Authentication failed");
-		break;
-
-	    case 2:
-		console.error("Server not configured for auth");
-		break;
-
-	    default:
-		console.error("Unknown error code for auth: " + data);
-
+    this.init = function() {
+	that.socket = io.connect(url);
+	that.socket.on("message", function(data) {
+	    if(typeof(that.events[data.e]) === "function")
+		that.events[data.e](data.p);
+	});
+	
+	//Internal states and error handling
+	that.socket.on("error", function(data) {
+	    if(typeof(console) !== "undefined") {
+		console.error(data);
 	    }
-	}
-    });
-    this.socket.on("chans", function(data) {
-	that.chans = data;
-    });
+	});
+	that.socket.on("auth", function(data) {
+	    if(typeof(console) !== "undefined") {
+		switch(data) {
+		    
+		case 0:
+		    console.log("Authenticated");
+		    break;
+		    
+		case 1:
+		    console.error("Authentication failed");
+		    break;
+		    
+		case 2:
+		    console.error("Server not configured for auth");
+		    break;
+		    
+		default:
+		    console.error("Unknown error code for auth: " + data);
+		    
+		}
+	    }
+	});
+	that.socket.on("chans", function(data) {
+	    that.chans = data;
+	});
+    }
+
+    //Include socketio
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.onload = this.init;
+    script.type = 'text/javascript';
+    script.src = url.replace(/\/$/, "") + "/socket.io/socket.io.js";
+    head.appendChild(script);
 
     //User functions
     this.on = function(event, callback) { 
