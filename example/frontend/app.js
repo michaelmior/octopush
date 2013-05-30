ac71e22a609bcea6ef811cbb077f7bd1451afd25
adisbladis <adis@blad.is>
2013-05-31 01:11:12 +0200

    Simplified example

--- example/frontend/app.js
+++
@@ -1,5 +0,0 @@
-var Push = new Octopush("http://localhost:3001");
-Push.on("humidity", function(data) {
-    $("div#messages").html("<p>Relative humidity is " + parseInt(data.humid) + "%</p>");
-});
-
