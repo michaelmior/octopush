528a645d84021b8a852f9019c9aaa0bbae51f830
Michael Mior <mike@joinbunch.com>
2013-06-05 09:20:53 -0400

    Normalize whitespace

--- usr/share/octopush/client.js
+++ usr/share/octopush/client.js
@@ -6,43 +6,43 @@ function Octopush(url) {
     //Base setup
     var that = this;
     this.init = function() {
-	that.socket = io.connect(url);
-	that.socket.on("message", function(data) {
-	    if(typeof(that.events[data.e]) === "function")
-		that.events[data.e](data.p);
-	});
-	
-	//Internal states and error handling
-	that.socket.on("error", function(data) {
-	    if(typeof(console) !== "undefined") {
-		console.error(data);
-	    }
-	});
-	that.socket.on("auth", function(data) {
-	    if(typeof(console) !== "undefined") {
-		switch(data) {
-		    
-		case 0:
-		    console.log("Authenticated");
-		    break;
-		    
-		case 1:
-		    console.error("Authentication failed");
-		    break;
-		    
-		case 2:
-		    console.error("Server not configured for auth");
-		    break;
-		    
-		default:
-		    console.error("Unknown error code for auth: " + data);
-		    
-		}
-	    }
-	});
-	that.socket.on("chans", function(data) {
-	    that.chans = data;
-	});
+        that.socket = io.connect(url);
+        that.socket.on("message", function(data) {
+            if(typeof(that.events[data.e]) === "function")
+                that.events[data.e](data.p);
+        });
+
+        //Internal states and error handling
+        that.socket.on("error", function(data) {
+            if(typeof(console) !== "undefined") {
+                console.error(data);
+            }
+        });
+        that.socket.on("auth", function(data) {
+            if(typeof(console) !== "undefined") {
+                switch(data) {
+
+                case 0:
+                    console.log("Authenticated");
+                    break;
+
+                case 1:
+                    console.error("Authentication failed");
+                    break;
+
+                case 2:
+                    console.error("Server not configured for auth");
+                    break;
+
+                default:
+                    console.error("Unknown error code for auth: " + data);
+
+                }
+            }
+        });
+        that.socket.on("chans", function(data) {
+            that.chans = data;
+        });
     }
 
     //Include socketio
@@ -54,20 +54,19 @@ function Octopush(url) {
     head.appendChild(script);
 
     //User functions
-    this.on = function(event, callback) { 
-	that.events[event] = callback;
+    this.on = function(event, callback) {
+        that.events[event] = callback;
     }
     this.join = function(channel) {
-	var obj = {chan: channel}
-	if(channel[0] === "!")
-	    obj.cookie = document.cookie
-	that.socket.emit("join", {chan: channel});
+        var obj = {chan: channel}
+        if(channel[0] === "!")
+            obj.cookie = document.cookie
+        that.socket.emit("join", {chan: channel});
     }
     this.part = function(channel) {
-	that.socket.emit("part", channel);
+        that.socket.emit("part", channel);
     }
     this.auth = function() {
-	that.socket.emit("auth", document.cookie);
+        that.socket.emit("auth", document.cookie);
     }
-
 }
